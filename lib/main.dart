90c71ec1f07a141f8fca74fb3b016b5960832e55
George Spanos <gespa11019@gmail.com>
2022-04-30 12:36:40 +0300

    flutter ci .8

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:boochat_ui/auth.dart';
 import 'package:boochat_ui/room-list/room_list.dart';
 import 'package:boochat_ui/shared/user.dart';
-import 'package:boochat_ui/shared/user_model.dart';
 import 'package:boochat_ui/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 Future main() async {
