e42317a2836ee19426ffe46a1072e5180eebf55f
George Spanos <gespa11019@gmail.com>
2022-05-08 15:24:31 +0300

    slight theming

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class BoochatTheme {
   static ThemeData get darkTheme {
-    return ThemeData.dark();
+    return ThemeData.dark().copyWith(
+        primaryColor: const Color.fromRGBO(24, 39, 57, 1),
+        scaffoldBackgroundColor: const Color.fromRGBO(12, 20, 45, 1),
+        hoverColor: const Color.fromRGBO(47, 64, 86, 1),
+        textTheme: Typography.material2014().white);
   }
 }
