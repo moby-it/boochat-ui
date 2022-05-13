3a09fb3227fa9695a21148ab391e93c9348f4e7a
George Spanos <gespa11019@gmail.com>
2022-05-13 03:13:23 +0300

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:boochat_ui/src/common/common.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   await dotenv.load(fileName: getEnvFilename());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
     if (kReleaseMode) exit(1);
